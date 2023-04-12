9101d0b9f5ae3676bc11abf73a3493406de23908
Dinar Wahyu Rahman <68122380+dinarrahman30@users.noreply.github.com>
2023-04-13 06:31:16 +0700

    Add files via upload

---
+++ Script/URLlink.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class URLlink : MonoBehaviour
+{
+    public void mozillahubsClick()
+    {
+        Application.OpenURL("https://hubs.mozilla.com/bFb7eXa/soulful-icy-world");
+    }
+}
